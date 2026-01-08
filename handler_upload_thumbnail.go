f739035b6c9d91be12c1ae38a89811d26d5c0310
Oleksii Kalinin <kaliha91@gmail.com>
2026-01-09 00:37:49 +0100

    Update handler_upload_thumbnail.go

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -27,11 +28,50 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
 		return
 	}
-
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	const maxMemory = 10 << 20
+
+	r.ParseMultipartForm(maxMemory)
+
+	thumbnailData, _, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error upploading thumbnail", err)
+		return
+	}
+
+	thumbnailFile, err := io.ReadAll(thumbnailData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error working with thumbnail", err)
+		return
+	}
+
+	videoMetaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error getting video", err)
+		return
+	}
+
+	if userID != videoMetaData.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Denied", err)
+		return
+	}
+
+	thumb := thumbnail{
+		data:      thumbnailFile,
+		mediaType: r.Header.Get("Content-Type"),
+	}
+
+	videoThumbnails[videoID] = thumb
+
+	thumbURL := fmt.Sprintf("/api/thumbnails/%s", videoID)
+	videoMetaData.ThumbnailURL = &thumbURL
+	err = cfg.db.UpdateVideo(videoMetaData)
+	if userID != videoMetaData.UserID {
+		respondWithError(w, http.StatusInternalServerError, "Unable to update thumbnail in DB", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, videoMetaData)
 }
