de7cb137d021e794e65bb0dcb13d0691ca33dc49
Oleksii Kalinin <kaliha91@gmail.com>
2026-01-12 11:21:03 +0100

    Update handler_upload_video.go

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -78,6 +78,17 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	}
 	defer videoData.Close()
 
+	videoMediaType := fh.Header.Get("Content-Type")
+	// Validate and extract extension
+	var ext string
+	switch videoMediaType {
+	case "video/mp4":
+		ext = "mp4"
+	default:
+		respondWithError(w, http.StatusBadRequest, "Invalid Content-Type. Allowed: video/mp4", nil)
+		return
+	}
+
 	tempVideo, err := os.CreateTemp("", "tubely-upload.mp4")
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "error temping the video", err)
@@ -93,7 +104,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	}
 	_, err = tempVideo.Seek(0, io.SeekStart)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "error seeking temp file", err)
+		respondWithError(w, http.StatusInternalServerError, "error getting video", err)
 		return
 	}
 
@@ -115,6 +126,20 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 
 	log.Printf("Video: %s, ratio: %s", videoID, aspect)
 
+	// Create moov optimized video
+	optimized, err := processVideoForFastStart(r.Context(), tempVideo.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error optimizing video", err)
+		return
+	}
+	newPath, err := os.Open(optimized)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error opening optimized video", err)
+		return
+	}
+	_, _ = newPath.Seek(0, io.SeekStart)
+	defer newPath.Close()
+
 	videoMetaData, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Error getting video", err)
@@ -130,17 +155,6 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	videoMediaType := fh.Header.Get("Content-Type")
-	// Validate and extract extension
-	var ext string
-	switch videoMediaType {
-	case "video/mp4":
-		ext = "mp4"
-	default:
-		respondWithError(w, http.StatusBadRequest, "Invalid Content-Type. Allowed: video/mp4", nil)
-		return
-	}
-
 	videoRandomBase := make([]byte, 32)
 	if _, err = rand.Read(videoRandomBase); err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Error generating video filename", err)
@@ -151,12 +165,18 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 
 	uploadCtx, cancel := context.WithTimeout(r.Context(), 5*time.Minute)
 	defer cancel()
+	info, err := newPath.Stat()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("processed file size: %d\n", info.Size())
+	newFileSize := info.Size()
 	_, err = cfg.s3Client.PutObject(uploadCtx, &s3.PutObjectInput{
 		Bucket:             &cfg.s3Bucket,
 		Key:                &videoFileName,
-		Body:               tempVideo,
+		Body:               newPath,
 		ContentType:        &videoMediaType,
-		ContentLength:      &fh.Size,
+		ContentLength:      &newFileSize,
 		ContentDisposition: aws.String("inline"),
 		CacheControl:       aws.String("public, max-age=31536000, immutable"),
 		ACL:                "public-read",
@@ -220,4 +240,29 @@ func getVideoAspectRatio(ctx context.Context, filePath string) (string, error) {
 		return "9:16", nil
 	}
 	return "other", nil
+}
+
+func processVideoForFastStart(ctx context.Context, filePath string) (string, error) {
+	buf := &bytes.Buffer{}
+	errBuf := &bytes.Buffer{}
+	var err error
+
+	log.Printf("Fasting video %s\n", filePath)
+
+	newPath := filePath + ".processing"
+
+	log.Printf("New video path: %s\n", newPath)
+
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "ffmpeg", "-i", filePath, "-c", "copy", "-movflags", "faststart", "-f", "mp4", newPath)
+	cmd.Stdout = buf
+	cmd.Stderr = errBuf
+
+	err = cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("ffmpeg failed: %w, stderr: %s", err, errBuf.String())
+	}
+	return newPath, nil
 }
